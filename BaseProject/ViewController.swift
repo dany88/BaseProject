50c940246ac5e5e39d0483cac3c225881e30b25a
Daniele Orrù <daniele.orru.dev@gmail.com>
2015-02-18 11:00:26 +0100

    Initial Commit

---
+++ BaseProject/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BaseProject
+//
+//  Created by Daniele Orrù on 13/02/15.
+//  Copyright (c) 2015 Daniele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
